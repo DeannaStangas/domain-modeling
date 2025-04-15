1e9c4fbfeab4f7def7e356bfd3d16aee77976f83
Deanna Stangas <deanna.stangas@kellogg.northwestern.edu>
2025-04-15 15:59:29 +0000

    crm example

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,36 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
--- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS me;
 
 -- CREATE TABLES
+CREATE TABLE activities(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+date TEXT,
+notes TEXT,
+contact_id INTEGER,
+type TEXT
+);
+
+CREATE TABLE contacts(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+email TEXT,
+phone TEXT,
+company_id INTEGER
+);
+
+CREATE TABLE companies(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT
+);
+
+CREATE TABLE me(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT,
+last_name TEXT,
+email TEXT
+);
